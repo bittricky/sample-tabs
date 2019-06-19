496e9bb8d2330c97439d03f3f11a3e529c5e6b12
Mitul Patel <39785177+bittricky@users.noreply.github.com>
2019-06-19 10:30:43 -0400

    app container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Tabs from "./components/Tabs/Tabs";
+import NAV_MENU from "./navigation.json";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Tabs menu={NAV_MENU}/>
     </div>
-  );
+  )
 }
 
 export default App;
